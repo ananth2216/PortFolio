b34754915b2c80b9c11361490b7417af4fd188b7
Ananth Vissapragada <ananthvissa99@gmail.com>
2021-05-23 18:11:53 +0530

    Removed navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { HeaderNavbarComponent } from './header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
